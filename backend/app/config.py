ab3dead0db3c08836712954464e46646ad09f3cb
showman228 <nekrasovaleksey.1999@gmail.com>
2026-01-21 13:29:50 +0300

    add config database

---
+++ backend/app/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings
+from typing import List, Union
+
+class Settings(BaseSettings):
+    app_name: str = "FastAPI TODO"
+    debug: bool = True
+    database_url: str = "sqlite:///./todo.db"
+    cors_origins: Union[List[str], str] = [
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:3000",
+    ]
+    static_dir: str = "static"
+    images_dir: str = "static/images"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
