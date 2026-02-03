e7242f36ca0d2ab6c8becc75d8842cdda5da9d14
showman228 <nekrasovaleksey.1999@gmail.com>
2026-02-03 18:17:28 +0300

    add tasks and users models

---
+++ backend/app/models/tasks.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, Text, Float, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime, UTC
+from ..database import Base
+# нужно придумать как связать с пользователем его задачу 
+class Task:
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False, index=True)
+    discription = Column(Text, nullable=True)
+    created_at = Column(DateTime(timezone=True), default=datetime.now(UTC))
+
+    users = relationship("User", back_populates="tasks")
+
+    def __repr__(self):
+        return f"<Task(id={self.id}, name={self.name}, description={self.description}, created_at={self.created_at})>"
---
+++ backend/app/models/user.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, Text, Float, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime, UTC
+from ..database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, nullable=False, index=True)
+    email = Column(String, nullable=False, index=True)
+    full_name = Column(String, nullable=True)
+    created_at = Column(DateTime(timezone=True), default=datetime.now(UTC))
+    priority_of_tasks = Column(Integer, nullable=True)
+
+    tasks = relationship("Tasks", back_populates="tasks")
+
+    def __repr__(self):
+        return f"<User(id={self.id}, username={self.username}, email={self.email})>"
