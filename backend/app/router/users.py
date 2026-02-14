5ceec62800874ad277969418180f3a96a27f6864
showman228 <nekrasovaleksey.1999@gmail.com>
2026-02-14 18:55:16 +0300

    update router users

--- backend/app/router/users.py
+++ backend/app/router/users.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from typing import List
 from backend.app.database import get_db
-from backend.app.schemas.users import UserResponse
+from backend.app.schemas.users import UserResponse, UserCreate, UserUpdate
 from backend.app.services.users import UserService
 
 router = APIRouter(
@@ -20,6 +20,18 @@ def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
     service = UserService(db)
     return service.get_user_by_id(user_id)
 
+@router.post("", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+def create_user(user_data: UserCreate, db: Session = Depends(get_db)):
+    service = UserService(db)
+    return service.create_user(user_data)
 
+@router.put("/{user_id}", response_model=UserResponse, status_code=status.HTTP_200_OK)
+def update_user(user_id: int, user_data: UserUpdate, db: Session = Depends(get_db)):
+    service = UserService(db)
+    return service.update_user(user_data, user_id)
 
-
+@router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    service = UserService(db)
+    service.delete_user(user_id)
+    return None
